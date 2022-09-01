bed1032ab2f07b1d52272c5a14cce7f44c377a69
Anderson Vieira <asvieira.br@gmail.com>
2022-09-01 11:01:12 -0300

    Cria rotas com os principais métodos HTTP

--- node-js/node-js-project/src/index.js
+++ node-js/node-js-project/src/index.js
@@ -4,6 +4,22 @@ const app = express();
 
 app.listen('7777');
 
-app.get('/', function (req, res) {
-    res.json({message: "Hello, world."})
+app.get('/courses', function (req, res) {
+    res.json(["Course 1", "Course 2", "Course 3"])
+})
+
+app.post('/courses', function(req, res) {
+    res.json(["Course 1", "Course 2", "Course 3", "Course 4"])
+})
+
+app.put('/courses/:id', function(req, res) {
+    res.json(["Course 5", "Course 2", "Course 3", "Course 4"])
+})
+
+app.patch('/courses/:id', function(req, res) {
+    res.json(["Course 5", "Course 6", "Course 3", "Course 4"])
+})
+
+app.delete('/courses/:id', function(req, res) {
+    res.json(["Course 6", "Course 3", "Course 4"])
 })
